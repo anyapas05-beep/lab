a4e25bea075517b27f7ca806e1dce74ffa401716
anyapas05-beep <anya.pas05@gmail.com>
2025-09-11 14:15:44 +0300

    Add files via upload

---
+++ pas.py
@@ -0,0 +1,31 @@
+#laba1
+
+name = 'Anna'
+age = 16
+height = 1.68
+student = True
+hobbies = ['sport']
+friends = {'Vika'}
+grades = (11, 10)
+info =  {'city': 'Lutsk'}
+
+#виведення кожної змінної та її типу
+
+print('name', type(name),':', name)
+print('age', type(age),':', age)
+print('height', type(height),':', height)
+print('student', type(student),':', student)
+print('hobbies', type(hobbies),':', hobbies)
+print('friends', type(friends),':', friends)
+print('grades', type(grades),':', grades)
+print('info', type(info),':', info)
+
+a = 12
+b = 3
+c = a + b                #додавання
+d = a - b                #віднімання
+i = a * b                 #множення
+f = a / b                 #діленння (результат завжди float)
+g = a // b                #ділення без остачі
+j = a % b                 #остача від ділення
+k = a ** b                #піднесення до степення
